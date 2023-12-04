631c5a3fdaadb094bb28e914f847054af859b10c
Pedro Falco <fpfalco@gmail.com>
2023-12-04 10:52:03 -0300

    Correct typo on paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     <main class="flex flex-col justify-center gap-4 sm:gap-10 mx-8 md:mx-20 md:flex md:flex-row md:justify-center md:gap-0">
         <div class="md:w-3/6 md:flex md:flex-col md:gap-8 md:justify-center">
             <h1 class="font-bold text-[48px] sm:text-[54px] break-words text-center leading-[90%] tracking-[-2.1px] md:text-left md:text-8xl md:tracking-[-4px] md:ml-0">Lychee<br>Protocol is<br>on the horizon</h1>
-            <p class="hidden font-bold md:text-2xl md:block">First DeFi super App</p>
+            <p class="hidden font-bold md:text-2xl md:block">The First DeFi Super App</p>
             <div class="hidden md:flex md:items-center md:gap-2">
                 <p class="text-white/50 md:text-xl">Powered by</p>
                 <img src="/layer-zero.svg" alt="Layer Zero">
@@ -22,7 +22,7 @@
         </div>
 
         <div class="md:hidden flex flex-col">
-            <p class="text-center font-bold text-2xl sm:text-3xl tracking-[-0.5px] md:hidden">First DeFi super App</p>
+            <p class="text-center font-bold text-2xl sm:text-3xl tracking-[-0.5px] md:hidden">The First DeFi Super App</p>
             
             <div class="flex items-center justify-center gap-2 mb-2 md:hidden">
                 <p class="text-white/50 text-l sm:text-xl font-normal ">Powered by</p>
